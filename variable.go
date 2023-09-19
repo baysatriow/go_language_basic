c4e3833753f0417c420abaae9738cf2dee8a5fe3
baysatriow <bayusatriowid@gmail.com>
2023-09-19 16:36:59 +0700

    feat: variable

--- variable.go
+++ variable.go
@@ -12,4 +12,9 @@ func main() {
 	var umur = 18
 
 	fmt.Println("Halo Nama Saya", nama_lengkap, ", Saya Lahir di", tempat_lahir, "Pada tanggal", tanggal_lahir, ", Umur Saya saat ini adalah", umur)
+
+	// Nilai dari variable dapat berubah ubah sesuah dengan alur program
+	umur += 2
+
+	fmt.Println("Saat ini tahun 2025 maka Umur saya adalah ", umur)
 }
