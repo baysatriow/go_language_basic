ce8f46066c9bb6060427d2f708e60068ea14c57a
baysatriow <bayusatriowid@gmail.com>
2023-09-19 16:35:15 +0700

    feat: dasar variable

---
+++ variable.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// Variable berfungsi untuk menyimpan sebuah data, dapat berupa integer, floating, string dll.
+
+	var nama_lengkap = "Bayu Satrio Wibowo"
+	var tanggal_lahir = "12 Maret 2005"
+	var tempat_lahir = "Pringsewu"
+	var umur = 18
+
+	fmt.Println("Halo Nama Saya", nama_lengkap, ", Saya Lahir di", tempat_lahir, "Pada tanggal", tanggal_lahir, ", Umur Saya saat ini adalah", umur)
+}
