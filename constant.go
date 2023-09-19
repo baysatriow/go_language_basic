2f6263744e5e958530cbac0b1351d4d4ac23c611
baysatriow <bayusatriowid@gmail.com>
2023-09-19 16:45:50 +0700

    feat: constant

---
+++ constant.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Constant berfungsi untuk menyimpan data dengan nilai yang konstan atau tetap dan tidak dapat berubah ubah bersifat global dapat di baca dengan program dari file lain
+
+	const alamat = "Wonosobo, Tanggamus, Lampung, Indonesia"
+	fmt.Println("Saya tinggal di", alamat)
+}
